3beb5b00e080f267adc127f153ed2f5e40df2699
brent.langston <brent.langston@Brent-Langstons-MacBook-Air-H9XJCWM62Q.local>
2026-02-06 17:59:07 -0600

    fix: correct SHA256 checksum for v2026.2.7

--- Formula/cco.rb
+++ Formula/cco.rb
@@ -18,7 +18,7 @@ class Cco < Formula
   # SHA256 values will be updated by CI after each release
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/visiquate/cco/releases/download/v2026.2.7/cco-aarch64-apple-darwin.tar.gz"
-    sha256 "cdf1e436c69d54d993cccce99c7dd40ed6cea4f6e6026092375bbaee2d3f3e47"
+    sha256 "feefad9e1f5bf7470713a4e854c8cd65549478d8a6d7ac2c80abf2253c134b0a"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/visiquate/cco/releases/download/v2025.12.58/cco-x86_64-unknown-linux-gnu.tar.gz"
     sha256 "84736d3bdab7c589ada2752b9eb2364ed5efcaf05970a9d2ad47becdbcd6ece1"
